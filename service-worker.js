26d9d9f2e238de09753770f4eefbef1cfb280352
rswebapps <r.s.kaientai@gmail.com>
2026-04-22 13:05:30 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // 戦略：Network First（常にネットワーク優先）
 // RC3以降の改修でservice-worker.jsの変更は原則不要
 
-const CACHE_NAME = 'shuan-rc8';
+const CACHE_NAME = 'shuan-rc9';
 const FALLBACK_URLS = [
   './index.html',
   './manifest.json',
